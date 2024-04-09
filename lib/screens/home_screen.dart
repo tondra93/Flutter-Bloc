47a439d0742fa28251dab0707ad39e14371d6f47
tondra93 <tondranahian@gmail.com>
2024-04-10 02:26:25 +0600

    home_screen updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -9,16 +11,6 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: const Text('Weather App'),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.white,
-          statusBarIconBrightness: Brightness.dark,
-        ),
-      ),
       body: Padding(
         padding:const EdgeInsets.fromLTRB(40,1.2*kToolbarHeight,40,20),
         child: SizedBox(
@@ -45,6 +37,34 @@ class HomeScreen extends StatelessWidget {
                     shape: BoxShape.circle,
                     color: Color(0xFF673AB7),
                   ),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0,-1.2),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFFFAB40),
+                  ),
+                ),
+              ),
+              BackdropFilter(filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.transparent),
+              ),),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Location',style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                    ),),
+                  ],
                 ),
               )
             ], ),
